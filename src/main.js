1725aab8b8c5b3dd80107e17b0b86215255f0037
pray-lee <praycis@gmail.com>
2018-11-07 17:51:28 +0800

    瞎比高

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
 import L from 'leaflet'
+import 'leaflet.chinatmsproviders'
 import '../node_modules/leaflet/dist/leaflet.css'
-import chinaProvider from 'leaflet.chinatmsproviders'
+import './style/index.css'
+// import './lib/osmbuilding-leaflet.js'
+//初始化地图
+let map = L.map('map', {
+  center: [31.59, 120.29],
+  zoom: 8
+})
+// 加载高德地图
+let Gaode = L.tileLayer.chinaProvider('GaoDe.Normal.Map', {
+  maxZoom: 20,
+  minZoom: 1
+})
+let Gaodimgem = L.tileLayer.chinaProvider('GaoDe.Satellite.Map', {
+  maxZoom: 20,
+  minZoom: 1
+})
+let Gaodimga = L.tileLayer.chinaProvider('GaoDe.Satellite.Annotion', {
+  maxZoom: 20,
+  minZoom: 1
+})
+var Gaodimage = L.layerGroup([Gaodimgem, Gaodimga]);
+
+const baseLayers = {
+  "高德地图": Gaode,
+  "高德卫星图": Gaodimage
+}
+// 2d底图切换
+L.control.layers(baseLayers, null).addTo(map)
+
+// 3d底图测试
