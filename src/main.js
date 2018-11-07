c7283ed6b09036078e800097450b5827018b78c0
pray-lee <praycis@gmail.com>
2018-11-07 14:14:43 +0800

    update loader

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
-import leaflet from 'leaflet'
-console.log(leaflet)
+import L from 'leaflet'
+import '../node_modules/leaflet/dist/leaflet.css'
+import chinaProvider from 'leaflet.chinatmsproviders'
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,45 @@
 var path = require('path')
+var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path:path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist')
   },
   mode: 'development',
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader'
-          }
-        ],
+        use: [{
+          loader: 'babel-loader'
+        }],
         exclude: [
           path.resolve(__dirname, 'node_modules')
         ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
+    }),
+    // source map options
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        sourceMap: true
+      }
     })
   ]
 }
